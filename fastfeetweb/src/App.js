1446867e6243abb6eb73f81de690aa80bf2bcd3b
Kelvin Lopes <kelvin7android@gmail.com>
2020-07-08 23:32:41 -0300

    Front End initial

---
+++ fastfeetweb/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://raw.githubusercontent.com/Rocketseat/bootcamp-gostack-desafio-03/master/.github/logo.png"  className="App-logo" alt="logo" />
+        <p>
+         FastFeet em construção aqui.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
